1e573a98def59548cc0d7ffe5c09af30f048cdce
Максим Скрипкин <max.skainet@yandex.ru>
2022-01-15 14:09:35 +0300

    Убрал пустю строку в конце

--- hw_python_oop/homework.py
+++ hw_python_oop/homework.py
@@ -1,7 +1,3 @@
-import math
-from typing import Sequence, Dict, List, Tuple, Set
-from dataclasses import dataclass
-
 class InfoMessage:
     """Информационное сообщение о тренировке"""
     def __init__(self,
@@ -14,21 +10,21 @@ class InfoMessage:
         self.duration = duration
         self.distance = distance
         self.speed = speed
-        self.calories = calories          
+        self.calories = calories
                  
     def get_message(self) -> str:
         """Информация о проделанной тренировке"""
         return(f'Тип тренировки: {self.training_type};'
-              f' Длительность: {self.duration:.3f} ч.;'
-              f' Дистанция: {self.distance:.3f} км;'
-              f' Ср. скорость: {self.speed:.3f} км/ч;'
-              f' Потрачено ккал: {self.calories:.3f}.')
+               f' Длительность: {self.duration:.3f} ч.;'
+               f' Дистанция: {self.distance:.3f} км;'
+               f' Ср. скорость: {self.speed:.3f} км/ч;'
+               f' Потрачено ккал: {self.calories:.3f}.')
 
 
 class Training:
     """Базовый класс тренировки."""
-    M_IN_KM = 1000 #константа для перевода значений из метров в километры
-    LEN_STEP = 0.65 #расстояние в шагах
+    M_IN_KM = 1000  # Константа для перевода значений из метров в километры
+    LEN_STEP = 0.65  # Расстояние в шагах
     def __init__(self,
                  action: int,
                  duration: float,
@@ -40,25 +36,22 @@ class Training:
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        dist = self.action * self.LEN_STEP / self.M_IN_KM
-        return dist
-        
+        return self.action * self.LEN_STEP / self.M_IN_KM
+               
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        get_speed = self.get_distance() / self.duration
-        return get_speed
-        
+        return self.get_distance() / self.duration
+                
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         pass 
 
     def show_training_info(self) -> InfoMessage:
         """Сообщения о результатах тренировки"""
-        message = InfoMessage(self.__class__.__name__, self.duration, self.get_distance(),
+        return InfoMessage(self.__class__.__name__, self.duration, self.get_distance(),
         self.get_mean_speed(), self.get_spent_calories())
-        return message
+       
          
-
 class Running(Training):
     """Тренировка: бег."""
     def __init__(self, action, duration, weight) ->None:
@@ -67,23 +60,21 @@ class Running(Training):
 
     def get_spent_calories(self) -> float:
         """Возвращает количество калорий за бег"""
-        #coeff_calorie_1 = 18, coeff_calorie_2 = 20
-        run_calor = (18 * self.get_mean_speed() - 20) * self.weight / self.M_IN_KM * (self.duration * 60)
-        return run_calor
-             
-    
+        # coeff_calorie_1 = 18, coeff_calorie_2 = 20
+        return (18 * self.get_mean_speed() - 20) * self.weight / self.M_IN_KM * (self.duration * 60)
+        
+                
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
     def __init__(self, action, duration, weight, height) ->None:
         """Инициализация атрибутов класса родителя"""
         super().__init__(action, duration, weight)
-        self.height = height #Доп параметр - рост
+        self.height = height  # Доп параметр - рост
 
     def get_spent_calories(self) :
         """Возвращает количество калорий за спорт ходьбу"""
-        walk_calor = (0.035 * self.weight + (self.get_mean_speed() ** 2 // self.height) * 
-         0.029 * self.weight) * self.duration * 60
-        return walk_calor
+        return (0.035 * self.weight + (self.get_mean_speed() ** 2 // self.height) * 
+        0.029 * self.weight) * self.duration * 60
         
           
 class Swimming(Training):
@@ -92,19 +83,16 @@ class Swimming(Training):
     def __init__(self, action, duration, weight, length_pool, count_pool) ->None:
         """Инициализация атрибутов класса родителя"""
         super().__init__(action, duration, weight)
-        self.length_pool = length_pool #длина бассейна
-        self.count_pool = count_pool  #сколько раз пользователь переплыл бассейн
+        self.length_pool = length_pool  # Длина бассейна
+        self.count_pool = count_pool   # Сколько раз пользователь переплыл бассейн
 
     def get_mean_speed(self) -> float:
         """Рассчитываем среднюю скорость"""
-        sw_speed = self.length_pool * self.count_pool / self.M_IN_KM / self.duration
-        return sw_speed
-
+        return self.length_pool * self.count_pool / self.M_IN_KM / self.duration
+        
     def get_spent_calories(self) -> float:
         """Метод вернёт кол-во калорий"""
-        sw_cal = (self.get_mean_speed() + 1.1) * 2 * self.weight
-        return sw_cal        
-
+        return (self.get_mean_speed() + 1.1) * 2 * self.weight
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
@@ -123,7 +111,6 @@ def main(training: Training) -> None:
     message = info.get_message()
     print(message)
 
-
 if __name__ == '__main__':
     packages = [
         ('SWM', [720, 1, 80, 25, 40]),
@@ -134,4 +121,3 @@ if __name__ == '__main__':
     for workout_type, data in packages:
         training = read_package(workout_type, data)
         main(training)
-      
